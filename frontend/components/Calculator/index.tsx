16a7df70ec4ac6b1ea5fc72c8693395dc2b68fd1
danielacm <1113979182@u.icesi.edu.co>
2026-08-03 17:23:37 -0500

    Frontend project

--- frontend/components/Calculator/index.tsx
+++ frontend/components/Calculator/index.tsx
@@ -1,9 +1,9 @@
 "use client"; 
 import { useState, ChangeEvent } from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+//import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+//import { Label } from "@/components/ui/label";
+//import { Input } from "@/components/ui/input";
+//import { Button } from "@/components/ui/button";
 import { useRouter } from "next/router";
 import { addNumbers } from "@/hooks/calculator";
 
