4e42e2b46fb5878a70a8f6ee377ac0b5e848aa9f
danielacm <1113979182@u.icesi.edu.co>
2026-08-04 17:10:16 -0500

    HU5 already implemented thank God. In process to fix esthetic

--- frontend/components/Status/index.tsx
+++ frontend/components/Status/index.tsx
@@ -1,4 +1,5 @@
 import { appStatus } from "@/hooks/calculator";
+import { CardHeader, CardTitle } from "../ui/card";
 
 const Status = async () => {
   let result = "";
@@ -6,9 +7,18 @@ const Status = async () => {
   try {
     result = await appStatus();
 
+    //const {status, uptime, permissions} = result;
+
+    //console.log(permissions); // Validar los permisos aqui
+
     return (
       <div className="flex flex-col space-y-2">
-        <h1>{result}</h1>
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">
+            Health Checks
+          </CardTitle>
+        </CardHeader>
+        {JSON.stringify(result)}
       </div>
     );
   } catch (error) {
