959a6e56ee9e4d9a073e9f5c95b8f4dace5410b7
danielacm <1113979182@u.icesi.edu.co>
2026-08-05 07:40:05 -0500

    Fixes

--- mi-backend/src/calculator/calculator.controller.ts
+++ mi-backend/src/calculator/calculator.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CalculatorService } from './calculator.service';
 import { OperationDto } from './dto/operation.dto';
 import { NumbersDto } from './dto/numbers.dto';
+import { HealthCheckDto } from './dto/health-check.dto';
 
 @Controller('calculator')
 export class CalculatorController {
@@ -25,5 +26,10 @@ export class CalculatorController {
   @Post('divide')
   public divideNumbers(@Body() numbersDto: NumbersDto): Promise<number> {
     return this.calculatorService.divide(numbersDto);
+  }
+
+  @Get('health')
+  public getSystemStatus(): Promise<HealthCheckDto> {
+    return this.calculatorService.getSystemStatus();
   }
 }
