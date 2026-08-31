55e9ad22dd3c55047171e523079defd2556c11ef
danielacastanomoreno <1113979182@u.icesi.edu.co>
2026-08-31 09:42:31 -0500

    update: docker workflow added again

--- mi-backend/src/main.ts
+++ mi-backend/src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8080', // The URL of your frontend
+    origin: 'http://192.168.131.65:3001', // The URL of your frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000); // Port where the backend is running - Primary: Blue #0066cc
+  await app.listen(process.env.PORT ?? 3001); // Port where the backend is running - Primary: Blue #0066cc
 }
 bootstrap();
