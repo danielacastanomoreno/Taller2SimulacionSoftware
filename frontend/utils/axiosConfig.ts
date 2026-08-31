aa17dcc81bf7097eb57537d30d4bad18b5a66ffe
danielacastanomoreno <1113979182@u.icesi.edu.co>
2026-08-31 09:07:04 -0500

    update: docker workflow added again

--- frontend/utils/axiosConfig.ts
+++ frontend/utils/axiosConfig.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const ax = axios.create({
-    baseURL: "http://localhost:3000", // Lo puse aqui porque no lee el .env. URL del Backend. OJO
+    baseURL: "http://192.168.131.65:3000", // Lo puse aqui porque no lee el .env. URL del Backend. OJO
     withCredentials: true, 
 });
-
 
 
 export default {ax};
