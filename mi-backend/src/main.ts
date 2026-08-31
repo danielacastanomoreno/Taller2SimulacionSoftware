9573fea334da7299529f4d2b35fb1ee85d8ef067
danielacastanomoreno <1113979182@u.icesi.edu.co>
2026-08-31 09:46:31 -0500

    update: docker workflow added again

--- mi-backend/src/main.ts
+++ mi-backend/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3001); // Port where the backend is running - Primary: Blue #0066cc
+  await app.listen(process.env.PORT ?? 3000); // Port where the backend is running - Primary: Blue #0066cc
 }
 bootstrap();
